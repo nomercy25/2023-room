393571a6cbc61f934d3fef26c58366e8b68df917
nomercy25 <91740728+nomercy25@users.noreply.github.com>
2023-11-29 19:49:02 +0200

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+        },
+        1200: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
+    },
+
+    // Autoplay
+
+    autoplay: {
+        delay: 5000,
+    },
+});
